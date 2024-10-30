a7a8e0934d308ae3e38796db64c8a1b83d29a567
aeffggbh <genuinedistinctclearcapital@gmail.com>
2024-10-29 22:15:51 -0300

    add ammo explanaition to how to play menu

--- asteroids/src/Scenes/how2play_scene.cpp
+++ asteroids/src/Scenes/how2play_scene.cpp
@@ -15,6 +15,7 @@ namespace HowToPlayScene
 	static Text::Text objectiveText;
 	static Text::Text powerUpsText;
 	static Text::Text specialEnemyText;
+	static Text::Text ammoText;
 	static Button::Button backToMenuButton;
 
 	void Init()
@@ -30,17 +31,20 @@ namespace HowToPlayScene
 		rightClickControl = Text::GetText(screenWidth / 2, leftClickControl.location.y + Text::GetTextHeight(leftClickControl) + static_cast<int>(Text::Padding::tiny), Text::Fonts::Default, static_cast<int>(Text::FontSize::small), "- Right click to BOOST MOVEMENT!", WHITE);
 		Text::CenterTextX(rightClickControl);
 
-		objectiveTitle = Text::GetText(screenWidth / 2, rightClickControl.location.y + Text::GetTextHeight(rightClickControl) + static_cast<int>(Text::Padding::medium), Text::Fonts::Title2, static_cast<int>(Text::FontSize::big), "OBJECTIVE", WHITE);
+		objectiveTitle = Text::GetText(screenWidth / 2, rightClickControl.location.y + Text::GetTextHeight(rightClickControl) + static_cast<int>(Text::Padding::small), Text::Fonts::Title2, static_cast<int>(Text::FontSize::big), "OBJECTIVES", WHITE);
 		Text::CenterTextX(objectiveTitle);
 
 		objectiveText = Text::GetText(screenWidth / 2, objectiveTitle.location.y + Text::GetTextHeight(objectiveTitle) + static_cast<int>(Text::Padding::small), Text::Fonts::Default, static_cast<int>(Text::FontSize::small), "- Destroy as many planets as you can!", WHITE);
 		Text::CenterTextX(objectiveText);
 
-		powerUpsText = Text::GetText(screenWidth / 2, objectiveText.location.y + Text::GetTextHeight(objectiveText) + static_cast<int>(Text::Padding::small), objectiveText.font, objectiveText.fontSize, "- Power-Up: Bullets with no collision!", WHITE);
+		powerUpsText = Text::GetText(screenWidth / 2, objectiveText.location.y + Text::GetTextHeight(objectiveText) + static_cast<int>(Text::Padding::small), objectiveText.font, objectiveText.fontSize, "- Power-Up: Remove all planets on screen!", WHITE);
 		Text::CenterTextX(powerUpsText);
 
 		specialEnemyText = Text::GetText(screenWidth / 2, powerUpsText.location.y + Text::GetTextHeight(powerUpsText) + static_cast<int>(Text::Padding::small), powerUpsText.font, powerUpsText.fontSize, "- Destroy the PLANET DEFENDERS!", WHITE);
 		Text::CenterTextX(specialEnemyText);
+
+		ammoText = Text::GetText(screenWidth / 2, specialEnemyText.location.y + Text::GetTextHeight(specialEnemyText) + static_cast<int>(Text::Padding::small), specialEnemyText.font, specialEnemyText.fontSize, "- Click LEFT CLICK once to RELOAD AMMO!", WHITE);
+		Text::CenterTextX(ammoText);
 
 		backToMenuButton = Button::GetButton(static_cast<float>(Text::Padding::tiny), screenHeight, 80, 40, "BACK", BLACK, YELLOW, WHITE, Text::Fonts::Default);
 		backToMenuButton.shape.y -= backToMenuButton.shape.height + static_cast<float>(Text::Padding::tiny);
@@ -62,6 +66,7 @@ namespace HowToPlayScene
 		Text::DrawText(objectiveText);
 		Text::DrawText(powerUpsText);
 		Text::DrawText(specialEnemyText);
+		Text::DrawText(ammoText);
 		Button::DrawButton(backToMenuButton);
 	}
 }
